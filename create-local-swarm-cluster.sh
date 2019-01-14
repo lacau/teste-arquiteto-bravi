8fa59ce50b48280daa4d328be51d6790faa04520
lacau <lacau@zpay.com.br>
2019-01-14 20:04:04 -0200

    Add verification for multiple network interfaces on swarm init.

--- create-local-swarm-cluster.sh
+++ create-local-swarm-cluster.sh
@@ -3,16 +3,23 @@
 # init swarm (need for service command); if not created
 docker node ls 2> /dev/null | grep "Leader"
 if [ $? -ne 0 ]; then
-  echo "Init swarm"
+  echo "Initializing swarm cluster"
   docker swarm init > /dev/null 2>&1
 fi
 
 # get join token
 SWARM_TOKEN=$(docker swarm join-token -q worker)
 
-# get Swarm master IP (Docker for Mac xhyve VM IP)
+# get Swarm master IP
 SWARM_MASTER=$(docker info --format "{{.Swarm.NodeAddr}}")
 echo "Swarm master IP: ${SWARM_MASTER}"
+
+# verify local ip
+if [ -z "$SWARM_MASTER" ]; then
+  echo "Cant choose a network to init swarm cluster. Go to Troubleshooting section in README.md"
+  exit 1
+fi
+
 sleep 10
 
 # show swarm cluster
