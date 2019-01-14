0a86ad995627db62f2fd330bd4158afddb42bd60
lacau <lacau@zpay.com.br>
2019-01-14 13:21:56 -0200

    Change run-local script to replace current version

--- run-local-docker.sh
+++ run-local-docker.sh
@@ -6,3 +6,5 @@ IMAGE_TAG="$IMAGE_NAME:v$VERSION.local"
 sed -i "s/\(.*image: current-version.*\)/    image: $IMAGE_TAG/g" docker-compose.yml
 
 docker stack deploy --compose-file docker-compose.yml braviapp
+
+sed -i "s/\(.*image: $IMAGE_TAG.*\)/    image: current-version/g" docker-compose.yml
